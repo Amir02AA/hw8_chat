b33344a3b8340becbf136676e7b59e0963dd06ed
amir aghighi <aghighiamir@yahoo.com>
2023-08-09 13:40:37 +0330

    add: add composer and yaml package and yaml file and a class for reading yaml

---
+++ back/config.php
@@ -0,0 +1,9 @@
+<?php
+include_once "vendor/autoload.php";
+use Symfony\Component\Yaml\Yaml;
+class config
+{
+    static function getMethod(){
+        return Yaml::parseFile("database_config.yaml");
+    }
+}
